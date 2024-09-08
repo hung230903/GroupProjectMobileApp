ce04d2386092637731a5c8c2a692cc5e1670cab3
Szonni <SZonny@proton.me>
2024-09-08 14:35:56 +0700

    Added select image from gallery (#3)

--- GroupProject/app/src/main/java/vn/edu/usth/groupproject/CameraActivity.java
+++ GroupProject/app/src/main/java/vn/edu/usth/groupproject/CameraActivity.java
@@ -1,19 +1,29 @@
 package vn.edu.usth.groupproject;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresExtension;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
+import android.provider.MediaStore;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 
 public class CameraActivity extends AppCompatActivity {
+    ImageButton captureButton, imagePicker;
+    Uri imgUri;
+    ActivityResultLauncher<Intent> resultLauncher;
 
-    private ImageButton captureButton, imagePicker;
 
+    @RequiresExtension(extension = Build.VERSION_CODES.R, version = 2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,17 +31,36 @@ public class CameraActivity extends AppCompatActivity {
 
         captureButton = findViewById(R.id.capture_button);
         captureButton.setOnClickListener(v -> {
-            Toast.makeText(this, "Capturing...", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(CameraActivity.this, InspectionActivity.class);
+            startActivity(intent);
+            finish();
         });
 
         imagePicker = findViewById(R.id.image_picker);
-        imagePicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CameraActivity.this, InspectionActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        imagePicker.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
+            resultLauncher.launch(intent);
         });
+        registerResult();
+
     }
+
+    private void registerResult() {
+        resultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+                        assert o.getData() != null;
+                        imgUri = o.getData().getData();
+                        Intent move = new Intent(CameraActivity.this, InspectionActivity.class);
+                        move.putExtra("imageUri", imgUri.toString());
+                        startActivity(move);
+                        finish();
+                    }
+                }
+        );
+    }
+
+
 }
--- GroupProject/app/src/main/java/vn/edu/usth/groupproject/InspectionActivity.java
+++ GroupProject/app/src/main/java/vn/edu/usth/groupproject/InspectionActivity.java
@@ -1,22 +1,34 @@
 package vn.edu.usth.groupproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InspectionActivity extends AppCompatActivity {
     ImageButton back;
+    ImageView img;
+    Uri imgUri;
+    String imgUriString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inspection);
+
+        Intent image = getIntent();
+        img = findViewById(R.id.img_inspection);
+        imgUriString = image.getStringExtra("imageUri");
+        if (imgUriString != null) {
+            imgUri = Uri.parse(imgUriString);
+            img.setImageURI(imgUri);
+        }
 
         back = findViewById(R.id.back_inspection);
         back.setOnClickListener(new View.OnClickListener() {
