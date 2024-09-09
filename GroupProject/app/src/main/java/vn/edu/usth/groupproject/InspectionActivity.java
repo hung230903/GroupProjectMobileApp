58e3fa4ab6f071a9489b94094ba541ccf68e9982
Szonni <SZonny@proton.me>
2024-09-09 23:51:13 +0700

    (#3) InspectionActivity Added dialog box to ask the user for the discard of the app camera image when the user press go back button

--- GroupProject/app/src/main/java/vn/edu/usth/groupproject/InspectionActivity.java
+++ GroupProject/app/src/main/java/vn/edu/usth/groupproject/InspectionActivity.java
@@ -1,27 +1,63 @@
 package vn.edu.usth.groupproject;
 
+import android.app.Dialog;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
 public class InspectionActivity extends AppCompatActivity {
     ImageButton back, upload;
     ImageView img;
     Uri imgUri;
     String imgUriString;
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inspection);
+
+        dialog = new Dialog(InspectionActivity.this);
+        dialog.setContentView(R.layout.discard_image_dialog);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(AppCompatResources.getDrawable(InspectionActivity.this, R.drawable.discard_dialog));
+
+        Button dialogCancel = dialog.findViewById(R.id.inspection_dialog_cancel);
+        Button dialogYes = dialog.findViewById(R.id.inspection_dialog_yes);
+
+        dialogCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialogYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteImage();
+                goBack();
+            }
+        });
+
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+
+            }
+        });
 
         Intent image = getIntent();
         img = findViewById(R.id.img_inspection);
@@ -33,18 +69,24 @@ public class InspectionActivity extends AppCompatActivity {
 
         back = findViewById(R.id.back_inspection);
         back.setOnClickListener(view -> {
+            // check if the image is from the app's camera or the gallery
             if (image.getStringExtra("name") != null) {
-                deleteImage();
+                dialog.show();
+            } else {
+                goBack();
             }
-            Intent intent = new Intent(InspectionActivity.this, CameraActivity.class);
-            startActivity(intent);
-            finish();
         });
 
         upload = findViewById(R.id.upload_inspection);
         upload.setOnClickListener(view -> {
             Toast.makeText(InspectionActivity.this, "Uploading...", Toast.LENGTH_SHORT).show();
         });
+    }
+
+    private void goBack() {
+        Intent intent = new Intent(InspectionActivity.this, CameraActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     private void deleteImage() {
