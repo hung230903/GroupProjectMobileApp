b31d1c1689f22ca562a199f51a0dbea6c5729cfa
Szonni <SZonny@proton.me>
2024-09-13 00:23:06 +0700

    Gallery Permission added Now the app will ask the user for gallery permission upon pressing the image selection icon

--- GroupProject/app/src/main/java/vn/edu/usth/groupproject/CameraActivity.java
+++ GroupProject/app/src/main/java/vn/edu/usth/groupproject/CameraActivity.java
@@ -15,6 +15,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresExtension;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
@@ -25,7 +26,9 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.os.ext.SdkExtensions;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -38,6 +41,7 @@ import vn.edu.usth.groupproject.databinding.ActivityObjectDetectionBinding;
 
 public class CameraActivity extends AppCompatActivity {
     private static final int CAMERA_PERMISSION_CODE = 1;
+    private static final int GALLERY_PERMISSION_CODE = 2;
     ImageButton captureButton, imagePicker;
     Uri imgUri;
     ActivityResultLauncher<Intent> resultLauncher;
@@ -46,14 +50,14 @@ public class CameraActivity extends AppCompatActivity {
     ImageCapture imageCapture;
 
 
-    @RequiresExtension(extension = Build.VERSION_CODES.R, version = 2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityObjectDetectionBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
+        registerCameraPermit();
+        registerResult();
 
         // Gradient Background Animation
         ConstraintLayout constraintLayout = findViewById(R.id.gradient_layout);
@@ -64,17 +68,43 @@ public class CameraActivity extends AppCompatActivity {
 
         // Image Buttons
         captureButton = findViewById(R.id.capture_button);
-        captureButton.setOnClickListener(v -> {
-            capture();
-        });
+        captureButton.setOnClickListener(v -> capture());
 
         imagePicker = findViewById(R.id.image_picker);
         imagePicker.setOnClickListener(v -> {
-            Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
-            resultLauncher.launch(intent);
+            if (this.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                registerGalleryPermit();
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && SdkExtensions.getExtensionVersion(Build.VERSION_CODES.R) >= 2) {
+                    openGallery();
+                }
+            }
         });
-        registerCameraPermit();
-        registerResult();
+    }
+
+    private void registerGalleryPermit() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(CameraActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, GALLERY_PERMISSION_CODE);
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Required")
+                    .setMessage("Please allow Files and Media permission in the app settings to choose image from gallery.")
+                    .setPositiveButton("Go to Settings", (dialog, which) -> {
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+                        intent.setData(uri);
+                        startActivity(intent);
+                    })
+                    .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                    .create()
+                    .show();
+        }
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.R, version = 2)
+    private void openGallery() {
+        Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
+        resultLauncher.launch(intent);
     }
 
     private void registerCameraPermit() {
@@ -88,10 +118,18 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == CAMERA_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                provideCamera();
-            }
+        switch (requestCode) {
+            case CAMERA_PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    provideCamera();
+                }
+                break;
+            case GALLERY_PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && SdkExtensions.getExtensionVersion(Build.VERSION_CODES.R) >= 2) {
+                        openGallery();
+                    }
+                }
         }
     }
 
