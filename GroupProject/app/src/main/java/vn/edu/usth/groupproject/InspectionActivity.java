3ac10ea228c053fa035ff14c36eba77c31154359
Szonni <SZonny@proton.me>
2024-11-03 15:10:35 +0700

    draw the image bbox based on returned values

--- GroupProject/app/src/main/java/vn/edu/usth/groupproject/InspectionActivity.java
+++ GroupProject/app/src/main/java/vn/edu/usth/groupproject/InspectionActivity.java
@@ -3,6 +3,11 @@ package vn.edu.usth.groupproject;
 import android.app.Dialog;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,10 +21,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -128,7 +135,7 @@ public class InspectionActivity extends AppCompatActivity {
 
         // Create the request
         Request request = new Request.Builder()
-                .url("http://192.168.1.8:8000/api/v1/detection")
+                .url("http://192.168.1.8:8000/api/v1/detection") // This works on my machine, I don't know about the others :v
                 .post(requestBody)
                 .addHeader("Content-Type", "multipart/form-data")
                 .build();
@@ -145,18 +152,62 @@ public class InspectionActivity extends AppCompatActivity {
                     assert response.body() != null;
                     String responseData = response.body().string();
                     JSONObject json;
+
+                    // Load the image into an immutable bitmap from uri
+                    InputStream inputStream = getContentResolver().openInputStream(imgUri);
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    // Convert immutable bitmap to mutable bitmap
+                    bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
                     try {
                         json = new JSONObject(responseData);
                     } catch (JSONException e) {
                         throw new RuntimeException(e);
                     }
                     System.out.println(json);
+                    // Get the bounding box from the json and draw it to a bitmap
+                    drawImageBoundingBox(json, bitmap);
+
+                    Bitmap finalBitmap = bitmap;
+                    // Display the result on the main thread
+                    runOnUiThread(() ->
+                            img.setImageBitmap(finalBitmap));
                     Logger.getLogger(OkHttpClient.class.getName()).setLevel(Level.FINE);
                 } else {
                     System.out.println("Request failed: " + response.message());
                 }
             }
         });
+    }
+
+    private void drawImageBoundingBox(JSONObject json, Bitmap bitmap) {
+        try {
+            JSONArray predictions = json.getJSONArray("predictions");
+
+            // Initiate canvas and paint to be able to draw
+            Canvas canvas = new Canvas(bitmap);
+            Paint paint = new Paint();
+            paint.setColor(Color.RED);
+            paint.setStrokeWidth(8);
+            paint.setStyle(Paint.Style.STROKE);
+
+            // Get the coordinates and draw
+            for (int i = 0; i < predictions.length(); i++) {
+                JSONObject prediction = predictions.getJSONObject(i);
+                JSONObject bbox = prediction.getJSONObject("bbox");
+                int x1 = bbox.getInt("x1");
+                int x2 = bbox.getInt("x2");
+                int y2 = bbox.getInt("y2");
+                int y1 = bbox.getInt("y1");
+
+                canvas.drawRect(x1, y1, x2, y2, paint);
+                System.out.println(x1 + " " + y1 + " " + x2 + " " + y2);
+            }
+
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // Create temp file to store the image information from the uri
